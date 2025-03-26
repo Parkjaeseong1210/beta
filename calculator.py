eeb9feccfdd8499d2686f73b9e6616089b85b589
Parkjaeseong1210 <lullaby1210@naver.com>
2025-03-26 14:25:53 +0900

    ...

---
+++ calculator.py
@@ -0,0 +1,15 @@
+
+
+select = int(input("1. 입력한 수식 계산  2. 두 수 사이의 합계 : "))
+
+if select == 1:
+    a = input("수식을 입력하세요")
+    b = eval(a)
+    print(" %s 결과는  %5.1f입니다." % (a, b))
+elif select == 2:
+    c = int(input("첫번째 숫자를 입력하세요 : "))
+    d = int(input("두번째 숫자를 입력하세요 : "))
+    e = int((c + d) * (d - c + 1)* 1/2)
+    print(e, "+...+", d, "는", e, "입니다")
+else :
+    print("1이나 2만 입력하세요")
