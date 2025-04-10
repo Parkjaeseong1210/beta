d96059fbf3cc4284c86b7c5b0d682516b592442f
Parkjaeseong1210 <lullaby1210@naver.com>
2025-04-10 22:32:23 +0900

    Add files via upload

--- 구구단.py
+++ 구구단.py
@@ -1,8 +1,8 @@
 for dan in range(2, 10):
-    print("#", dan, "단", end="\t")
+    print(f"#  {dan}단  #", end = "")
 print()
 
-for i in range(1, 10):         
-    for dan in range(2, 10): 
-        print(dan, "X", i, "=", dan * i, end="\t")
+for i in range(1, 10):
+    for dan in range(2,10):
+        print(f"{dan}X{i} = {dan*i:2}", end = " ")
     print()
